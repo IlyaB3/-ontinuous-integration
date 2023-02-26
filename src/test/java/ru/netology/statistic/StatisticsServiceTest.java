a78eea7c777c60ca83579cdef6759f778ef21a08
IlyaB <ilyaberd3@gmail.com>
2023-02-26 21:31:09 +0300

    main2

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,12 +10,11 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 14, 15, 17, 17, 17, 14, 6, 11, 11, 12};
+        long[] incomesInBillions = {14, 15, 17, 17, 15, 16, 8, 6, 12, 11, 12};
         long expected = 17;
 
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
     }
-
 }
